7cb795594f94ec9b1f6d4e375d4099f9dabc996d
ChunyuBai <baichunyu26@gmail.com>
2023-02-03 18:32:09 -0500

    setup react

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react'
+
+function App() {
+  const [users,setUsers] = useState([]);
+  useEffect(()=>{
+    axios.get('/users').then(res => {
+      console.log(res.data.users);
+      setUsers(res.data.users)
+    })
+  },[])
+  return (
+    <div className="App">
+      <h2>hello world</h2>
+    </div>
+  );
+}
+
+export default App;
