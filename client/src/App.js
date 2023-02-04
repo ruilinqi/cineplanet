d0752c7fa8da08106a684670033fd549159496b3
ChunyuBai <baichunyu26@gmail.com>
2023-02-04 10:49:35 -0500

    setup react

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,15 @@ function App() {
   const [users,setUsers] = useState([]);
   useEffect(()=>{
     axios.get('/users').then(res => {
-      console.log(res.data.users);
-      setUsers(res.data.users)
+      console.log(res.data.users[0].email);
+      let result = res.data.users;
+      setUsers(result)
     })
   },[])
   return (
     <div className="App">
       <h2>hello world</h2>
+      <h2>{Object.keys(users).map(user => <li>{users[user].email}</li>)}</h2>
     </div>
   );
 }
