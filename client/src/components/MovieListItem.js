605f24d478bf7a29f39e9c7486c3c63d6f64238b
ruilinqi <ruilinqi7@gmail.com>
2023-02-05 10:52:26 -0500

    Add movie card

--- client/src/components/MovieListItem.js
+++ client/src/components/MovieListItem.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState,useEffect } from "react";
 import axios, { all } from "axios";
+import MovieCard from "./MovieCard";
 export default function MovieListItem(props) {
   const [movies,setMovies] = useState(null);
   const [trailer,setTrailer]= useState("")
   useEffect(()=> {
-  const movieURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=5147ae78ba442bfacb0dba2cfe66bf6f";
+  const movieURL = `https://api.themoviedb.org/3/movie/top_rated?api_key=5147ae78ba442bfacb0dba2cfe66bf6f`;
   // const trailerURL = "https://api.themoviedb.org/3/movie/76600/videos?api_key=5147ae78ba442bfacb0dba2cfe66bf6f";
 
   Promise.all([
@@ -23,12 +24,11 @@ export default function MovieListItem(props) {
   return (
     <div>
      {movies?<>
-    {movies.map(movie => <li key = {movie.id}>
-      Title:{movie.title}
-      Vote Average:{movie.vote_average}
-      Release Date:{movie.release_date}
-      <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}/>
-      </li>)}
+    {movies.map((movie => 
+      <li>
+        <MovieCard key={movie.id} {...movie}/>      
+      </li>
+    ))}
     </>:null}
     </div>
   )
