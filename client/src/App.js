872ecf88376401d8e6299cd537585086e606249f
ruilinqi <ruilinqi7@gmail.com>
2023-02-06 10:54:48 -0500

    Add user login files

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,55 @@
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import {useState, useEffect} from 'react'
 import MovieListItem from './components/MovieListItem';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Logout from "./components/Logout";
+
 function App() {
-  const [users,setUsers] = useState([]);
-  useEffect(()=>{
-    axios.get('/users').then(res => {
-      let result = res.data.users;
-      setUsers(result)
-    })
-  },[])
+  // const [users,setUsers] = useState([]);
+
+  // useEffect(()=>{
+  //   axios.get('/users').then(res => {
+  //     let result = res.data.users;
+  //     setUsers(result)
+  //   })
+  // },[])
+
   return (
     <div className="App">
       <h2>hello world</h2>
-      <h2>{users.map(user => <li key = {user.id}>{user.email}</li>)}</h2>
-      <MovieListItem/>
+      {/* <div>
+        <div className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to={"/register"} className="nav-link">
+              Sign up
+            </Link>
+          </li>
+        </div>
+      </div> */}
+      
+      {/* <Router>
+    <Routes>
+    <Route path="/login" component={<Login/>} />
+    </Routes>
+    <Routes>
+    <Route component={<MovieListItem/>} />
+    </Routes>
+  </Router> */}
+        {/* <h2>{users.map(user => <li key = {user.id}>{user.email}</li>)}</h2> */}
+        <Login/>
+        <MovieListItem/>
+
     </div>
   );
 }
