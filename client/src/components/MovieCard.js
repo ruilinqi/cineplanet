653df94cc748b79fe6c962d06c145fb8c174e511
ruilinqi <ruilinqi7@gmail.com>
2023-02-13 12:42:26 -0500

    Fix movie card margin

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -22,11 +22,11 @@ const MovieCard = ({ title, vote_average, release_date, poster_path, overview })
             <figure className="image">
               <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} onClick={handleClick} />
             </figure>
-            <div className="content">
+            <div className="content" style={{marginTop:"0px"}}>
               Title: {title} <br />
               Vote Average: {vote_average} <br />
               Release Date: {release_date} <br />
-              <button className="button-more-details" onClick={() => setOpenModal(true)}>More Details</button>
+              <button className="button-more-details" onClick={() => setOpenModal(true)} style={{fontSize:"20px", padding:"5px 0"}} >More Details</button>
             </div>
           </div>
         </div>
